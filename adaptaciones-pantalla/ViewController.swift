54ac933ded7450b0acb55cd0849af94918180bfa
analourdescr <ana_lourdes98@hotmail.com>
2022-08-25 07:30:54 -0700

    Initial Commit

---
+++ adaptaciones-pantalla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adaptaciones-pantalla
+//
+//  Created by Alumno on 8/25/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
